d54a54eccad725e7ed10f586b348cb0ede513e5a
AdrianTomasDiez2C <adrian_tomas_bc3dv1mc2021@cev.com>
2022-03-24 17:44:20 +0100

    Anim + final mov mando

--- Assets/Scripts/NewPlayerMovement.cs
+++ Assets/Scripts/NewPlayerMovement.cs
@@ -7,12 +7,15 @@ public class NewPlayerMovement : MonoBehaviour
 {
     Playercontrols controls;
     private Rigidbody rb;
-    public float speed = 1f;
+    public float speed = 0.7f;
     public Vector2 inputVector;
+    Animator animator;
+    private bool Moving;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
 
         controls = new Playercontrols();
         controls.Gameplay.Move.performed += Movement_performed;
@@ -24,7 +27,7 @@ public class NewPlayerMovement : MonoBehaviour
         Debug.Log(obj);
         Vector2 inputVector2 = obj.ReadValue<Vector2>();
         float speed2 = 0.5f;
-        rb.AddForce(new Vector3(inputVector2.x, 0f, 0f) * speed, ForceMode.Force);
+        rb.AddForce(new Vector3(inputVector2.x, 0f, 0f) * speed2, ForceMode.Force);
     }
     // Start is called before the first frame update
     void Start()
@@ -32,22 +35,33 @@ public class NewPlayerMovement : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         inputVector = controls.Gameplay.Move.ReadValue<Vector2>();
-        rb.AddForce(new Vector3(inputVector.x, 0f, 0f) * speed, ForceMode.VelocityChange);
+        rb.velocity = (new Vector3(inputVector.x, 0f, 0f) * speed);
         controls.Gameplay.Move.canceled += ctx => inputVector = Vector2.zero;
-    }
+        controls.Gameplay.Move.canceled += ctx => Moving = false;
+        controls.Gameplay.Move.performed += ctx => Moving = true;
 
+
+        if (Moving == true)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else if (Moving == false)
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+    // Update is called once per frame
 
     void OnEnable()
     {
-        controls.Gameplay.Enable();
+        controls.Gameplay.Move.Enable();
     }
 
     private void OnDisable()
     {
-        controls.Gameplay.Disable();
+        controls.Gameplay.Move.Disable();
     }
 }
