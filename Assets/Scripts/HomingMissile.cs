70ba4e00cedf92565d8f71e751b05ee7e20d7d35
AdrianTomasDiez2C <adrian_tomas_bc3dv1mc2021@cev.com>
2022-04-22 16:03:51 +0200

    e

--- Assets/Scripts/HomingMissile.cs
+++ Assets/Scripts/HomingMissile.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class HomingMissile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform target;
+    public Rigidbody2D rigidBody;
+    public float angleChangingSpeed;
+    public float movementSpeed;
+    void FixedUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector2 direction = (Vector2)target.position - rigidBody.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+        rigidBody.angularVelocity = -angleChangingSpeed * rotateAmount;
+        rigidBody.velocity = transform.up * movementSpeed;
     }
 }
