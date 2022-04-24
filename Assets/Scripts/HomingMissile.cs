424e67a5478cf43f7b5bf1b76cc71233bee3d4e8
FluxxNova <89316911+FluxxNova@users.noreply.github.com>
2022-04-24 23:01:45 +0200

    HomingFix1

--- Assets/Scripts/HomingMissile.cs
+++ Assets/Scripts/HomingMissile.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class HomingMissile : MonoBehaviour
 {
+   
+  
     public Transform target;
     public Rigidbody2D rigidBody;
     public float angleChangingSpeed;
     public float movementSpeed;
+    public GameObject Player;
+    void start()
+    {
+        GameObject Player = GameObject.FindWithTag("Player");
+        target = Player.transform;
+    }
     void FixedUpdate()
     {
         Vector2 direction = (Vector2)target.position - rigidBody.position;
